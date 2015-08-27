75bcddd32a87653426d4d3579202a740fc424e83
Jack Somervell <jacksomervell@gmail.com>
2015-08-27 20:29:14 +0100

    done but alas they had to put in the second line

--- rubycode.rb
+++ rubycode.rb
@@ -8,11 +8,11 @@ puts "You have selected " + start_line
 
 puts "Which station are you starting at?"
 start_station = gets.chomp
-puts "You are starting at " + start_station + "on the " + start_line + "line"
+puts "You are starting at " + start_station + " on the " + start_line 
 
-# puts "Which line do you want to get to?"
-# end_line = gets.chomp
-# puts "You have selected " + end_line
+puts "Which line do you want to get to?"
+end_line = gets.chomp
+puts "You have selected " + end_line
 
 puts "Which station do you want to get to?"
 end_station = gets.chomp
@@ -40,9 +40,15 @@ puts "You are going to " + end_station
 
 
 startline_symbol = start_line.to_sym
+endline_symbol = end_line.to_sym
 
 if MTA[startline_symbol].count(end_station) == 0
-  puts "fish"
+  leg_one = MTA[startline_symbol].index("US")  - MTA[startline_symbol].index(start_station)
+  leg_one = leg_one.abs
+  leg_two = MTA[endline_symbol].index(end_station) - MTA[endline_symbol].index("US")
+  leg_two = leg_two.abs
+  stop_amount = leg_one + leg_two
+  puts "Your journey will take " + stop_amount.to_s + " stops"
 end
 
 
